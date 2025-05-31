158c4b862374bcad3e8f244db5e9a05d97d076a5
nilay <nilay.pophalkar@acc.ltd>
2025-05-31 18:17:30 +0530

    opengraph sharable link metadata

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/join", async (req, res) => {
     res.render('opengraph')
 })
 
+app.get('/', async (req, res) => {
+    res.send("Hello there!")
+})
+
 app.listen(5000, () => {
     console.log("App running on port 5000")
 })
