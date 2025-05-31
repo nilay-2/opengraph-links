35c216f20aec008c37b8d51b75932a3782efc530
nilay <nilay.pophalkar@acc.ltd>
2025-05-31 17:10:44 +0530

    opengraph sharable link metadata

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get("/join", async (req, res) => {
+    res.render('opengraph')
+})
+
+app.listen(5000, () => {
+    console.log("App running on port 5000")
+})
